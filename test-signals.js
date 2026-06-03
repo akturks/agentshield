f210f09a40c0ce6e37f492da5b480f528c5cea36
Serdar Aktürk <serdar@Serdar-Mac-mini.local>
2026-06-04 00:07:15 +0300

    Implement signal engine v1

---
+++ test-signals.js
@@ -0,0 +1,26 @@
+import {
+  findOrCreateIdentity
+} from "./repositories/identityRepository.js";
+
+import db from "./repositories/db.js";
+
+import {
+  deriveSignals
+} from "./repositories/signalRepository.js";
+
+const identity = findOrCreateIdentity({
+  tenantId: "tenant_1",
+  fingerprint: "curl/8.0"
+});
+
+const events = db.prepare(`
+  SELECT *
+  FROM Event
+  WHERE identityId = ?
+`).all(identity.id);
+
+const signals =
+  deriveSignals(events);
+
+console.log(signals);
+
